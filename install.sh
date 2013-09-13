b20d5d5f8e667c4bb2ac82ecfa6cae1f53cb92e1
Donggu Kang <donggu@pentamidine.csail.mit.edu>
2013-09-13 16:41:33 -0400

    auto install prozto

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
