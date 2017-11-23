0f134d73e55e0b512eda8e54458c101c78d4cb8f
elbaro <elbaro@users.noreply.github.com>
2017-11-23 18:20:41 +0900

    vim for ubuntu

--- install-vim.sh
+++ install-vim.sh
@@ -1,4 +1,9 @@
-pacman -S --noconfirm --needed neovim vim-plug git python2-neovim python-neovim xclip xsel python-jedi
+if [ -x "$(command -v apt-get)" ]; then
+  apt-get install -y neovim vim-plug git python2-neovim python-neovim xclip xsel python-jedi
+elif [ -x "$(command -v pacman)" ]; then
+  pacman -S --noconfirm --needed neovim vim-plug git python2-neovim python-neovim xclip xsel python-jedi
+fi
+
 mkdir -p ~/.config/nvim/colors
 
 pwd="$(pwd -P)"
@@ -6,4 +11,3 @@ pwd="$(pwd -P)"
 ln -sf $pwd/vim/vimrc ~/.config/nvim/init.vim
 ln -sf $pwd/vim/peaksea.vim ~/.config/nvim/colors/peaksea.vim
 nvim --headless +PlugInstall +qa
-
