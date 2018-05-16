84330eb7107d6123cd0bbf6229e5e6bb3bd6c660
elbaro <elbaro@users.noreply.github.com>
2018-05-16 17:39:01 +0900

    No custom color script is required for alacritty

--- install-color.sh
+++
@@ -1,6 +0,0 @@
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    bash <(curl -s https://raw.githubusercontent.com/elbaro/starting-block/master/blob/snazzy-gnome.sh)
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # Mac OSX
-    open <(curl -s "https://raw.githubusercontent.com/sindresorhus/iterm2-snazzy/master/Snazzy.itermcolors")
-fi
