786f30301066670b9b11f491d3368cc148612353
elbaro <elbaro@users.noreply.github.com>
2018-05-18 04:54:05 +0900

    cleanup

--- install-nvim.sh
+++ install-nvim.sh
@@ -2,9 +2,9 @@ if [ -x "$(command -v apt-get)" ]; then
     apt-get install software-properties-common
     add-apt-repository -y ppa:neovim-ppa/stable
     apt-get update
-    apt-get install -y neovim git xclip xsel python-jedi
+    apt-get install -y neovim python-jedi
     pip3 install neovim
-    pip install neovim
+    
     curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -12,15 +12,12 @@ if [ -x "$(command -v apt-get)" ]; then
     update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
     update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 elif [ -x "$(command -v pacman)" ]; then
-    pacman -S --noconfirm --needed neovim vim-plug git python2-neovim python-neovim xclip xsel python-jedi
+    pacman -S --noconfirm --needed neovim vim-plug python-neovim python-jedi
     pacman -R --noconfirm vim vi
     pacman -S --noconfirm --needed neovim-drop-in
 fi
 
-mkdir -p ~/.config/nvim/colors
-
 pwd="$(pwd -P)"
 
 ln -sf $pwd/vim/vimrc ~/.config/nvim/init.vim
-ln -sf $pwd/vim/peaksea.vim ~/.config/nvim/colors/peaksea.vim
 nvim --headless +PlugInstall +qa
